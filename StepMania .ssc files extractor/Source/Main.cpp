69e0d239df670499f40b24518880d53ab5311ff0
MarCza1992 <marcus43@wp.pl>
2025-04-04 14:58:25 +0200

    added .sm file support

--- StepMania .ssc files extractor/Source/Main.cpp
+++ StepMania .ssc files extractor/Source/Main.cpp
@@ -30,6 +30,8 @@ struct Chart
 
 void DisplayChartsData(const vector<Chart>& baza)
 {
+	cout << "Chart_ID" << "|" << "Song_Title" << "|" << "Song_Subtitle" << "|" << "Song_Artist" << "|" << "Song_Length" << "|" << "Song_BPM" << "|" << "Technical_Notation" << "|" << "Difficulty_Name" << "|" << "Difficulty_Rating" << "|" << "Step_Artist" << "|" << "Amount_of_Notes" << "|" << "Amount_of_LongNotes" << "|" << "Amount_of_Rolls" << "|" << "Amount_of_Mines" << "Pack" << endl;
+
 	for (size_t i = 0; i < baza.size(); i++)
 	{
 		cout << baza[i].ID << "|" << baza[i].title << "|" << baza[i].subtitle << "|" << baza[i].artist << "|" << baza[i].length << "|" << baza[i].BPM << "|" << baza[i].technotation << "|" << baza[i].difficulty << "|" << baza[i].rating << "|" << baza[i].charter << "|" << baza[i].Notes << "|" << baza[i].LongNotes << "|" << baza[i].Rolls << "|" << baza[i].Mines << "|" << baza[i].pack << endl;
@@ -90,7 +92,7 @@ int main()
 	
 	if (!filesystem::exists(SourceFolder) || !filesystem::is_directory(SourceFolder))
 	{
-		cout << "Folder 'Packs' nie istnieje" << endl;
+		cout << "Folder 'Packs' not found!" << endl;
 		return 1;
 	}
 
@@ -105,7 +107,7 @@ int main()
 				if (filesystem::is_directory(ChartFolder))
 				{
 					string ChartName = ChartFolder.path().filename().string();
-					string sscpath, oggpath;
+					string sscpath, oggpath, smpath;
 
 					for (const auto& file : filesystem::directory_iterator(ChartFolder))
 					{
@@ -118,6 +120,11 @@ int main()
 						{
 							oggpath = file.path().string();
 						}
+
+						if (file.path().extension() == ".sm")
+						{
+							smpath = file.path().string();
+						}
 					}
 
 					if (!oggpath.empty())
@@ -129,7 +136,7 @@ int main()
 						}
 						else
 						{
-							cout << "Brak danych audio w pliku: " << PackName << " - " << ChartName << endl;
+							cout << "Audio data not found in file: " << PackName << " - " << ChartName << endl;
 						}
 					}
 					
@@ -141,12 +148,11 @@ int main()
 						plik.open(sscpath, ios::in);
 						if (!plik.is_open())
 						{
-							cout << "pliku nie znaleziono" << endl;
+							cout << ".ssc file not found" << endl;
 							return 1;
 						}
 
 						string linia;
-						int checkcredits = 0;
 						int checkbpm = 0;
 
 						while (getline(plik, linia))
@@ -383,16 +389,9 @@ int main()
 
 							if (linia.find("#CREDIT:") == 0)
 							{
-								if (checkcredits == 0)
-								{
-									checkcredits++;
-								}
-								else
-								{
-									size_t start = linia.find("#CREDIT:") + 8;
-									size_t end = linia.length() - 1;
-									chart.charter = linia.substr(start, end - start);
-								}
+								size_t start = linia.find("#CREDIT:") + 8;
+								size_t end = linia.length() - 1;
+								chart.charter = linia.substr(start, end - start);
 
 								if (chart.charter.find("|") != string::npos)
 								{
@@ -414,6 +413,11 @@ int main()
 										break;
 									}
 
+									if (linia.find("measure") != string::npos)
+									{
+										continue;
+									}
+
 									for (char c : linia)
 									{
 										switch (c)
@@ -433,12 +437,331 @@ int main()
 								chart.Rolls = RollsAmount;
 								chart.Mines = MinesAmount;
 
+								if (chart.charter.empty())
+								{
+									chart.charter = chart.technotation;
+									chart.technotation = " ";
+								}
+
+								if (chart.charter == chart.technotation)
+								{
+									chart.technotation = " ";
+								}
+
 								BazaChartow.push_back(chart);
 								chart = Chart();
 							}
 						}
 						plik.close();
+					}
 
+					else
+					{
+						fstream plik;
+
+						plik.open(smpath, ios::in);
+						if (!plik.is_open())
+						{
+							cout << ".sm file not found" << endl;
+							return 1;
+						}
+
+						string linia;
+
+						while (getline(plik, linia))
+						{
+							if (linia.find("#TITLE:") == 0)
+							{
+								chart.ID = currentID++;
+
+								chart.pack = PackName;
+
+								size_t start = linia.find("#TITLE:") + 7;
+								size_t end = linia.length() - 1;
+								chart.title = linia.substr(start, end - start);
+
+								if (chart.title.find("|") != string::npos)
+								{
+									chart.title.erase(chart.title.find("|"), 1);
+								}
+							}
+
+							if (linia.find("#TITLETRANSLIT:") == 0)
+							{
+								size_t start = linia.find("#TITLETRANSLIT:") + 15;
+								size_t end = linia.length() - 1;
+								if (linia.substr(start, (end - start) + 1) != ";")
+								{
+									chart.title = linia.substr(start, end - start);
+								}
+
+								if (chart.title.find("|") != string::npos)
+								{
+									chart.title.erase(chart.title.find("|"), 1);
+								}
+							}
+
+							if (linia.find("#SUBTITLE:") == 0)
+							{
+								size_t start = linia.find("#SUBTITLE:") + 10;
+								size_t end = linia.length() - 1;
+								if (linia.substr(start, (end - start) + 1) == ";")
+								{
+									chart.subtitle = " ";
+								}
+								else
+								{
+									chart.subtitle = linia.substr(start, end - start);
+								}
+
+								if (chart.subtitle.find("|") != string::npos)
+								{
+									chart.subtitle.erase(chart.subtitle.find("|"), 1);
+								}
+							}
+
+							if (linia.find("#SUBTITLETRANSLIT:") == 0)
+							{
+								size_t start = linia.find("#SUBTITLETRANSLIT:") + 18;
+								size_t end = linia.length() - 1;
+								if (linia.substr(start, (end - start) + 1) != ";")
+								{
+									chart.subtitle = linia.substr(start, end - start);
+								}
+
+								if (chart.subtitle.find("|") != string::npos)
+								{
+									chart.subtitle.erase(chart.subtitle.find("|"), 1);
+								}
+							}
+
+							if (linia.find("#ARTIST:") == 0)
+							{
+								size_t start = linia.find("#ARTIST:") + 8;
+								size_t end = linia.length() - 1;
+								chart.artist = linia.substr(start, end - start);
+
+								if (chart.artist.find("|") != string::npos)
+								{
+									chart.artist.erase(chart.artist.find("|"), 1);
+								}
+							}
+
+							if (linia.find("#ARTISTTRANSLIT:") == 0)
+							{
+								size_t start = linia.find("#ARTISTTRANSLIT:") + 16;
+								size_t end = linia.length() - 1;
+								if (linia.substr(start, (end - start) + 1) != ";")
+								{
+									chart.artist = linia.substr(start, end - start);
+								}
+
+								if (chart.artist.find("|") != string::npos)
+								{
+									chart.artist.erase(chart.artist.find("|"), 1);
+								}
+							}
+
+							if (linia.find("#DISPLAYBPM:") == 0)
+							{
+								size_t start = linia.find("#DISPLAYBPM:") + 12;
+								size_t end = linia.length() - 1;
+
+								if (linia.substr(start, (end - start) + 1) == ";")
+								{
+									continue;
+								}
+
+								chart.BPM = linia.substr(start, end - start);
+
+								if (chart.BPM.find(":") != string::npos)
+								{
+									chart.BPM.replace(chart.BPM.find(":"), 1, " - ");
+								}
+							}
+
+							if (linia.find("#BPMS:") == 0)
+							{
+								if (chart.BPM.empty())
+								{
+
+									float tempo = -1;
+									float MINBPM = -1;
+									float MAXBPM = -1;
+									float current = -1;
+
+									size_t start = linia.find("=") + 1;
+									size_t end = linia.length();
+									tempo = stof(linia.substr(start, end - start));
+
+
+									while (getline(plik, linia))
+									{
+
+										if (linia.find(";") != string::npos)
+										{
+											break;
+										}
+
+										size_t start = linia.find("=") + 1;
+										size_t end = linia.length();
+										current = stof(linia.substr(start, end - start));
+
+										if (current < tempo)
+										{
+											if (MINBPM == -1)
+											{
+												MINBPM = current;
+											}
+											if (current < MINBPM)
+											{
+												MINBPM = current;
+											}
+										}
+										else
+										{
+											if (MAXBPM == -1)
+											{
+												MAXBPM = current;
+											}
+											if (current > MAXBPM)
+											{
+												MAXBPM = current;
+											}
+										}
+									}
+
+									if (MAXBPM != -1 || MINBPM != -1)
+									{
+										if (MAXBPM == -1)
+										{
+											chart.BPM = to_string(MINBPM) + " - " + to_string(tempo);
+										}
+										else
+										{
+											chart.BPM = to_string(tempo) + " - " + to_string(MAXBPM);
+										}
+									}
+
+									if (MAXBPM != -1 && MINBPM != -1)
+									{
+										chart.BPM = to_string(MINBPM) + " - " + to_string(MAXBPM);
+									}
+
+									if (MAXBPM == -1 && MINBPM == -1)
+									{
+										chart.BPM = to_string(tempo);
+									}
+								}
+							}
+
+							if (linia.find("#CREDIT:") == 0)
+							{
+								size_t start = linia.find("#CREDIT:") + 8;
+								size_t end = linia.length() - 1;
+								chart.charter = linia.substr(start, end - start);
+
+								if (chart.charter.find("|") != string::npos)
+								{
+									chart.charter.erase(chart.charter.find("|"), 1);
+								}
+							}
+
+							if (linia.find("//---") == 0)
+							{
+								if (chart.title.empty())
+								{
+									chart.ID = currentID++;
+									chart.title = BazaChartow.back().title;
+									chart.subtitle = BazaChartow.back().subtitle;
+									chart.artist = BazaChartow.back().artist;
+									chart.length = BazaChartow.back().length;
+									chart.BPM = BazaChartow.back().BPM;
+									chart.pack = BazaChartow.back().pack;
+								}
+
+								if (chart.charter.empty())
+								{
+									size_t start = linia.find("//---") + 32;
+									size_t end = linia.length() - 16;
+									chart.charter = linia.substr(start, end - start);
+								}
+							}
+
+							if (linia.find("#NOTES:") == 0)
+							{
+								int NotesAmount = 0;
+								int LongNotesAmount = 0;
+								int RollsAmount = 0;
+								int MinesAmount = 0;
+								int numerlinii = 1;
+
+								while (getline(plik, linia))
+								{
+									if (numerlinii == 2)
+									{
+										size_t start = 5;
+										size_t end = linia.length() - 1;
+
+										if (linia.substr(start, (end - start) + 1) == ":")
+										{
+											numerlinii++;
+											continue;
+										}
+
+										chart.charter = linia.substr(start, end - start);
+									}
+
+									if (numerlinii == 3)
+									{
+										size_t start = 5;
+										size_t end = linia.length() - 1;
+										chart.difficulty = linia.substr(start, end - start);
+									}
+
+									if (numerlinii == 4)
+									{
+										size_t start = 5;
+										size_t end = linia.length() - 1;
+										chart.rating = stoi(linia.substr(start, end - start));
+									}
+
+									if (linia.find(";") != string::npos)
+									{
+										break;
+									}
+
+									if (linia.find("measure") != string::npos)
+									{
+										continue;
+									}
+
+									for (char c : linia)
+									{
+										switch (c)
+										{
+										case '1': NotesAmount++; break;
+										case '2': LongNotesAmount++; break;
+										case '4': RollsAmount++; break;
+										case 'M': MinesAmount++; break;
+										case '0': break;
+										case '3': break;
+										default: break;
+										}
+									}
+									numerlinii++;
+								}
+								chart.Notes = NotesAmount;
+								chart.LongNotes = LongNotesAmount;
+								chart.Rolls = RollsAmount;
+								chart.Mines = MinesAmount;
+								chart.technotation = " ";
+
+								BazaChartow.push_back(chart);
+								chart = Chart();
+							}
+						}
+						plik.close();
 					}
 				}
 			}
@@ -473,6 +796,5 @@ int main()
 		}
 	} while (option != 3);
 
-	cin.get();
 	cin.get();
 }
